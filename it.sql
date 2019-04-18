899faad7423d5c9098acb86cc51afd9f8eef8235
ShadowUltimatez <49733155+ShadowUltimatez@users.noreply.github.com>
2019-04-18 07:45:24 +0200

    Delete it.sql

--- it.sql
+++
@@ -1,9 +0,0 @@
--- [IT]
-
-USE nomeDatabase;
-
-CREATE TABLE IF NOT EXISTS `user_whitelist` (
-  `identificatore` VARCHAR(25) CHARACTER SET utf8 NOT NULL,
-  `whitelistato` BOOLEAN NOT NULL DEFAULT FALSE,
-  PRIMARY KEY (`identificatore`)
-) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
